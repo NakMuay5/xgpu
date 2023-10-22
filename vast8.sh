4f7c05e15ef24702860b092533db88f0f6378d33
NakMuay5 <90797500+NakMuay5@users.noreply.github.com>
2023-10-22 08:33:34 -0700

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xccE9f36a5e56A0B5fEEEb4107Fcb1c2D22D66e17/g' config.conf
 sleep 5
 echo "---------------------------"
 
